37be8426e9b8aeb79c97a18e66ef33cd6c2c9f57
SolMaria02-ux <maparipiapa.02@gmail.com>
2026-07-10 23:18:39 -0500

    style: rediseña todo el sistema con identidad visual propia

--- web/config/settings.py
+++ web/config/settings.py
@@ -76,5 +76,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
